f6b33dd552888be98ee762cf7af86de1e1878293
tarporta <tarporta@gmail.com>
2026-05-28 16:08:30 +0900

    first upload

---
+++ android/app/src/main/java/com/motionfocus/tarporta/MainActivity.java
@@ -0,0 +1,5 @@
+package com.motionfocus.tarporta;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.motionfocus.tarporta',
+  appName: 'motionfocus',
+  webDir: 'dist'
+};
+
+export default config;
